a1c8154a67abc364e299c2bbc155998054d04a14
Ashley Pean <pean.ashley@gmail.com>
2021-05-10 06:42:30 -0400

    remove stack navigtor components

--- client/App.js
+++ client/App.js
@@ -1,27 +1,18 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native' 
 import { createStackNavigator } from '@react-navigation/stack'
+import StateProvider from './State/context'
 import Auth from './components/Auth/Auth'
 import Login from './components/Login/Login'
 import Register from './components/Register/Register'
-import QR from './components/QR/QR'
-import Home from './components/Home/Home'
-import StateProvider from './State/context'
-
-
+import Menu from './components/Menu/Menu'
 
 export default function App() {
   const Stack = createStackNavigator();
   return (
     <StateProvider>
       <NavigationContainer>
-        <Stack.Navigator headerMode={false}>
-          {/* <Stack.Screen name="auth" component={Auth} />
-          <Stack.Screen name="login" component={Login} />
-          <Stack.Screen name="register" component={Register} />
-          <Stack.Screen name="qr" component={QR} /> */}
-          <Stack.Screen name="home" component={Home} />
-        </Stack.Navigator>
+        <Menu />
       </NavigationContainer>
     </StateProvider>
   );
