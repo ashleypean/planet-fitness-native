01b2b2721723d3f632d871ff8a6e545953d26291
Ashley Pean <pean.ashley@gmail.com>
2021-05-10 03:33:34 -0400

    add support for naming of different app views

--- client/components/Nav/Nav.js
+++ client/components/Nav/Nav.js
@@ -1,10 +1,31 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useContext } from 'react'
+import { StateContext } from '../../State/context'
+import styled from 'styled-components/native'
+import { Appbar } from 'react-native-paper'
 
-export default function Nav({componentName}) {
+export default function Nav ({ component }) {
+  const { state: { user } } = useContext(StateContext)
+
   return (
-    <View>
-      <Text>{componentName}</Text>
-    </View>
+    <NavContainer>
+      <Appbar.Action 
+        icon="menu"
+        color="white"
+        onPress={() => console.log('pressed')}/>
+
+      <Appbar.Content
+        color="white" 
+        title={component}/>
+
+      <Appbar.Action 
+        icon="tune"
+        color="white"
+        onPress={() => console.log('pressed')}/>
+    </NavContainer>
   )
 }
+
+const NavContainer = styled(Appbar)`
+  background: transparent;
+  color: white;
+`
